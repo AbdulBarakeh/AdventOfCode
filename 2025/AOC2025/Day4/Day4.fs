f2aade03a7e76be9feac91f01c80101a411ccf8d
Abdul <AbdulBarakeh98@gmail.com>
2025-12-08 21:22:42 +0100

    loop with exceptions to handle outOfBound

--- 2025/AOC2025/Day4/Day4.fs
+++ 2025/AOC2025/Day4/Day4.fs
@@ -1,15 +1,63 @@
 module Day4
+
 let run() = 
 
     printfn "Hello from F# Day4"
-
-    let inputRaw = System.IO.File.ReadAllText(@$"C:\Repositories\AdventOfCode\2025\AOC2025\Day4\example.txt")
+    let mutable paperRollsAccessable = 0
+    let inputRaw = System.IO.File.ReadAllText(@$"C:\Users\Abdul-mbb\Documents\Repo\AdventOfCode\2025\AOC2025\Day4\input.txt")
     // Convert to matrix
     printfn "%s" inputRaw
     let input = inputRaw.Split('\n')
 
     let matrix= input|> Array.map(fun v -> v.ToCharArray())
         
-    // // for row in matrix do
-    // //     printfn "%s" (System.String row)
-    matrix[6][2] |> printfn "The element: %c"
+    for i in 0 .. matrix.Length - 1 do
+        for j in 0 .. matrix[i].Length - 1 do
+            let mutable count = 0
+            if matrix[i][j] = '@' then
+                
+                try
+                    if matrix[i][j+1] = '@' then 
+                        count <- count + 1
+                with _ -> ()
+                try 
+                    if matrix[i][j-1] = '@' then 
+                        count <- count + 1
+                with _ -> ()
+
+                try
+                    if matrix[i-1][j] = '@' then 
+                        count <- count + 1
+                with _ -> ()
+
+                
+                try
+                    if matrix[i+1][j] = '@' then 
+                        count <- count + 1
+                with _ -> ()
+
+                try
+                    if matrix[i-1][j+1] = '@' then 
+                        count <- count + 1
+                with _ -> ()
+
+                try
+                    if matrix[i-1][j-1] = '@' then 
+                        count <- count + 1
+                with _ -> ()
+                
+                try
+                    if matrix[i+1][j+1] = '@' then 
+                        count <- count + 1
+                with _ -> ()
+
+                try
+                    if matrix[i+1][j-1] = '@' then 
+                        count <- count + 1
+                with _ -> ()
+                
+                if count < 4 then
+                    paperRollsAccessable <- paperRollsAccessable + 1
+                
+    printfn  "Paper Count: %d" paperRollsAccessable
+    0
