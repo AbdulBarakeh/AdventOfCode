53752498615c2e28876bc82a8bd09c4c7d1672f2
Abdul-Rahman Abdurrohman <ara@ece.au.dk>
2025-12-11 14:20:49 +0100

    complete day 6

--- 2025/AOC2025/Day6/Day6.fs
+++ 2025/AOC2025/Day6/Day6.fs
@@ -2,15 +2,27 @@ module Day6
 let run() = 
 
     printfn "Hello from F# Day6"
-    let inputRaw = System.IO.File.ReadAllText(@$"C:\Users\Abdul-mbb\Documents\Repo\AdventOfCode\2025\AOC2025\Day6\example.txt")
+    let inputRaw = System.IO.File.ReadAllText(@$"C:\Repositories\AdventOfCode\2025\AOC2025\Day6\input.txt")
     // Convert to matrix
-    printfn "%s" inputRaw
     let matrix = inputRaw.Split('\n') 
                 |> Array.map(fun x -> x.Trim())          
                 |> Array.map(fun x -> x.Split(" ") |> Array.filter(fun x -> x <> "") |> Array.map string)
-    printfn "%A" matrix
     let mutable result = []
 
     for i in 0 .. matrix[0].Length - 1 do
+        let mutable tempRes = []
         for j = 0 to matrix.Length - 1 do
-            matrix[j][i] |> printf "%s "
+            tempRes <- matrix[j][i] :: tempRes
+        if tempRes[0] = "+" then
+            let tempoRes = tempRes|> List.tail |> List.map(fun x -> System.Int64.Parse(x))
+            result<- (tempoRes |> List.sum)::result
+            else
+                printfn "%A" tempRes
+                let tempoRes = tempRes|> List.tail|> List.map(fun x -> System.Int64.Parse(x))
+                let mutable resNum = 1:int64
+                for identifier in tempoRes do
+                    resNum <- resNum*identifier
+                result<- resNum::result
+                printfn "%A" result
+
+    result |> List.sum |> printfn "%d"
